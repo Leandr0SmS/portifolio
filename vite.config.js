7f84226928ce6cf95b640c90a23ec394909ab766
Leandro Simoes <leandrosimoesms@gmail.com>
2025-04-16 16:53:13 -0300

    feat: Polyfill crypto

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import crypto from 'crypto-browserify';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    'global.crypto': crypto,
+  },
   base: '/portifolio/',
-})
+});
