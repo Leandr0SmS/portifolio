c89f2b1c1fbb4bcff0a53e21c525320eb9d2ca68
Leandro Simoes <leandrosimoesms@gmail.com>
2025-04-16 17:05:24 -0300

    update Node build

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import crypto from 'crypto-browserify';
 
 export default defineConfig({
   plugins: [react()],
-  define: {
-    'global.crypto': crypto,
-  },
   base: '/portifolio/',
 });
