797e64b5e10e1ee13f4aabdbd558cd99f4cd7ca7
LeandroSmS <leandrosimoesms@gmail.com>
2025-04-17 06:35:46 -0300

    Revert "update Node build"
    This reverts commit c89f2b1c1fbb4bcff0a53e21c525320eb9d2ca68.

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import crypto from 'crypto-browserify';
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',
+  define: {
+    'global.crypto': crypto,
+  },
+  base: '/portifolio/',
 });
