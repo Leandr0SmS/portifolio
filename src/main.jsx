c7d2cc16daed0ed56bbd188591b30cc110f7f4bd
LeandroSmS <leandrosimoesms@gmail.com>
2026-03-26 16:25:41 -0300

    Added SSR and sitemap.xml

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,33 @@
 import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { createRoot, hydrateRoot } from 'react-dom/client'
+import { renderToString } from 'react-dom/server'
 import './styles/main.scss';
 import App from './App.jsx'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+export async function prerender() {
+  const html = renderToString(
+    <StrictMode>
+      <App />
+    </StrictMode>
+  );
+  return { html };
+}
+
+if (typeof document !== 'undefined') {
+  const root = document.getElementById('root');
+
+  if (root.hasChildNodes()) {
+    hydrateRoot(
+      root,
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  } else {
+    createRoot(root).render(
+      <StrictMode>
+        <App />
+      </StrictMode>
+    );
+  }
+}
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    vitePrerenderPlugin({
+      prerenderRoutes: ['/'],
+    }),
+    {
+      name: 'force-exit',
+      closeBundle() {
+        setTimeout(() => process.exit(0), 1000);
+      },
+    },
+  ],
   base: '/',
 });
