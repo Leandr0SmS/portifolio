e84266e622fe3dd0415409bb274520c15863d78a
LeandroSmS <leandrosimoesms@gmail.com>
2026-03-31 14:38:40 -0300

    Fix: Removed SSR

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { renderToString } from 'react-dom/server'
 import './styles/main.scss';
 import App from './App.jsx'
-
-export async function prerender() {
-  const html = renderToString(
-    <StrictMode>
-      <App />
-    </StrictMode>
-  );
-  return { html };
-}
 
 if (typeof document !== 'undefined') {
   createRoot(document.getElementById('root')).render(
--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { vitePrerenderPlugin } from 'vite-prerender-plugin';
 
 export default defineConfig({
   plugins: [
     react(),
-    vitePrerenderPlugin({
-      prerenderRoutes: ['/'],
-    }),
-    {
-      name: 'force-exit',
-      closeBundle() {
-        setTimeout(() => process.exit(0), 1000);
-      },
-    },
   ],
   base: '/',
 });
